e140f4dad57096c6a80dd68da92ddebbe0a6bb19
Ervin1809 <Mervink1809@gmail.com>
2025-12-20 02:34:44 +0800

    FIX : Add Model Relationship and Fillable

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Image extends Model
 {
-    //
+    protected $fillable = [
+        'title',
+        'file_low_res',
+        'file_high_res',
+    ];
+
+    public function reviews(){
+        return $this->hasMany(Review::class);
+    }
+    public function user(){
+        return $this->belongsToMany(User::class, 'reviews');
+    }
+
 }
